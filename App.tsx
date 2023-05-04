ac83b7c3cd23473f8a3ddb34ba8fe07a00074994
Yan Carlos <yan.amorim@grupohpar.com.br>
2023-05-04 16:45:49 -0400

    Login

--- App.tsx
+++ App.tsx
@@ -1,118 +1,39 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {Text, TextInput, View} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see  your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+const Login = () => {
+    return (
+      <View style={{ flex:1, justifyContent:"center", alignItems:"center", backgroundColor:"#fff", paddingHorizontal:16 }}>
+        <View style={{ justifyContent:"space-evenly", alignItems:"flex-start", width:"100%", padding:10, height:250 }}>
+          <Text 
+          style={{ fontSize:24, fontWeight:600, color:"#000" }}
+          >
+            Login
+          </Text>
+        <TextInput 
+        placeholder='Login'
+        style={{ borderRadius: 4, width:"100%", height:40, borderWidth:0.5, paddingHorizontal:16 }}>
+        </TextInput>
+        <TextInput 
+        placeholder='Senha'
+        style={{ borderRadius: 4, width:"100%", height:40, borderWidth:0.5, paddingHorizontal:16 }}>
+        </TextInput>
+        <View style={{ backgroundColor:"#0ec7ff", width:"100%", justifyContent:"center", alignItems:"center", padding:10, borderRadius:4}}>
+        <Text 
+          style={{ fontSize:18, fontWeight:600, color:"#fff" }}
+          >
+            Entrar
+          </Text>
         </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+          </View>
+      </View>
+    )
 }
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+const App = () => {
+return(
+  <Login></Login>
+)
+}
 
 export default App;
